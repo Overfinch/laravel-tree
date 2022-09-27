fd3f7bf51dd22a8555d1a206aca86922befc5dd8
Overfinch <bundichmail@gmail.com>
2022-09-27 09:47:34 +0300

    Update tree2.blade.php

--- tree2.blade.php
+++ tree2.blade.php
@@ -1,10 +1,10 @@
-<ol class="dd-list">
+<ul>
     @foreach($categories as $category)
-        <li class="dd-item" data-id="{{$category->id}}">
-            <div class="dd-handle">{{$category->name}}</div>
+        <li>
+            {{$category->name}}
             @if(!empty($category->categories))
                 @include('tree2',['categories' => $category->categories])
             @endif
         </li>
     @endforeach
-</ol>
+</ul>
